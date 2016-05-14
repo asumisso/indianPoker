0a5de9deb55956ea87aba66c675e6c72b7b60917
asumisso <karaage.imps@gmail.com>
2016-05-14 15:51:43 +0900

    aa

--- js/indian.js
+++ js/indian.js
@@ -1,15 +1,11 @@
 var deck = [];
 var suit = ["HEART", "DIA", "CLUB", "SPADE"];
 
-deckPush(); //デッキをプッシュ
-
-
+deckPush(deck); //デッキをプッシュ
 
 var me = [];
 var you = [];
 
-draw(me);
-draw(you);
 //デバッグ用
 console.log(me);
 console.log(you);
@@ -19,7 +15,7 @@ deckPush(deck); //デッキをプッシュ
 var me;
 var you;
 
-draw(me, you);
+battle(me, you);
 // >>>>>>> 5f6707572bd7547a8d1c9cc7c6d71acc0ca55625
 
 shuffle(deck);
