df65afc47de3afc3dcdc4d6b6d78491269a41bfa
asumisso <karaage.imps@gmail.com>
2016-05-15 01:47:33 +0900

    DOM操作,フォーム作成,トランプの見た目いじった

--- js/function.js
+++ js/function.js
@@ -1,9 +1,10 @@
-<<<<<<< HEAD
+
+
 function deckPush(array) {
-    // >>>>>>> 5f6707572bd7547a8d1c9cc7c6d71acc0ca55625
-=======
-function deckPush(deck) {
->>>>>>> 1caf8bbac8bf4bf7b9c1e248696183515740b7ff
+
+    var deck = [];
+    var suit = ["HEART", "DIA", "CLUB", "SPADE"];
+
 
     for (var i = 0; i < suit.length; i++) {
         for (var j = 0; j < 13; j++) {
@@ -22,33 +23,19 @@ function shuffle(array) {
     for (var i = 0; i < 15; i++) {
         var rand = Math.floor(Math.random() * array.length);
         console.log(array[rand]); //デバッグ
-<<<<<<< HEAD
 
-        var topnum = document.createElement("span");
-        node.setAttribute('class','topnum');
-
-        var topsuit = document.createElement("span");
-        node.setAttribute('class','topsuit');
-
-        var btmsuit = document.createElement("span");
-        node.setAttribute('class','btmsuit');
-
-        var btmnum = document.createElement("span");
-        node.setAttribute('class','btmnum');
-
-=======
->>>>>>> 1caf8bbac8bf4bf7b9c1e248696183515740b7ff
     }
-
 }
+
+    function draw(name) {
+        var rand = Math.floor(Math.random() * deck.length);
+        name.push(deck[rand]);
+        deck.splice(rand, 1);
+    }
 
 function battle(me, you) {
 
-  function draw(name) {
-    var rand = Math.floor(Math.random() * deck.length);
-    name.push(deck[rand]);
-    deck.splice(rand, 1);
-  }
+
     draw(me);
     draw(you);
 
@@ -61,8 +48,142 @@ function battle(me, you) {
     } else {
         document.write("おかしい");
     }
-<<<<<<< HEAD
+
+    //デバッグ用
+    console.log(me[0].num);
+    console.log(you[0].num);
+
+    //DOM操作
+    createHTML();
 }
-=======
-  }
->>>>>>> 1caf8bbac8bf4bf7b9c1e248696183515740b7ff
+
+function numAndSuit(person){
+
+    if (person.suit == "DIA") {
+        person.suit = "♦";
+    }else if (person.suit == "HEART") {
+        person.suit = "♥";
+    }else if (person.suit == "CLUB") {
+        person.suit = "♣";
+    }else{
+        person.suit = "♠";
+    }
+
+    if (person.num == 12) {
+        person.num = "K";
+    }else if (person.num == 11) {
+        person.num = "Q";
+    }else if (person.num == 10){
+        person.num = "J";
+    }else{
+        person.num = person.num + 1;
+    }
+
+}
+
+
+function createHTML(){
+    //本当はHTMLに直接かきたかったんだけど、入れ子になってるidは撮ってこれないみたい
+    //だから、createElementして、HTMLを追加していった
+    //functionでmeとyouうごかしたかったんだけど、できなかったらベタ書きしてます
+    //meとyou同じことしてる
+
+    //me
+
+    numAndSuit(me[0]);
+    numAndSuit(you[0]);
+
+    //div1のidをとってきてる
+    var catchMeid = document.getElementById("me");
+
+    //formを作ってdiv1に追加 
+    var meForm = document.createElement("form");
+    meForm.setAttribute("id","meForm");
+    catchMeid.appendChild(meForm);
+
+    //inputを作って↑に追加
+    var meInput = document.createElement("input");
+    meInput.setAttribute("id","meInput");
+    meInput.setAttribute("type","text");
+    meInput.setAttribute("value","myName");
+    var catchForm =document.getElementById("meForm");
+    catchForm.appendChild(meInput);
+
+    //divをつくってdiv1に追加
+    var meDiv = document.createElement("div");
+    meDiv.setAttribute("id","meDiv");
+    meDiv.setAttribute("class","card");
+    catchMeid.appendChild(meDiv);
+
+
+    //↑のidをとってきてる
+    var catchMediv = document.getElementById("meDiv");
+
+    //span(topnum用)をつくってmeDivに追加
+    var meTopnum = document.createElement("span");
+    meTopnum.setAttribute("class","topnum");
+    meTopnum.innerHTML = me[0].num;
+    catchMediv.appendChild(meTopnum);
+
+    //span(topsuit用)をつくってmeDivに追加
+    var meTopsuit = document.createElement("span");
+    meTopsuit.setAttribute("class","topsuit");
+    meTopsuit.innerHTML = me[0].suit;
+    catchMediv.appendChild(meTopsuit);
+
+    //span(btmsuit用)をつくってmeDivに追加
+    var meBtmsuit = document.createElement("span");
+    meBtmsuit.setAttribute("class","btmsuit");
+    meBtmsuit.innerHTML = me[0].suit;
+    catchMediv.appendChild(meBtmsuit);
+
+    //span(btmnum用)をつくってmeDivに追加
+    var meBtmnum = document.createElement("span");
+    meBtmnum.setAttribute("class","btmnum");
+    meBtmnum.innerHTML = me[0].num;
+    catchMediv.appendChild(meBtmnum);
+
+
+
+    //you
+    var catchYouid = document.getElementById("you");
+
+    var youForm = document.createElement("form");
+    youForm.setAttribute("id","youForm");
+    catchYouid.appendChild(youForm);
+
+    var youImput = document.createElement("input");
+    youImput.setAttribute("id","youImput");
+    youImput.setAttribute("type","text");
+    youImput.setAttribute("value","yourName");
+    var catchForm =document.getElementById("youForm");
+    catchForm.appendChild(youImput);
+
+    var youDiv = document.createElement("div");
+    youDiv.setAttribute("id","youDiv");
+    youDiv.setAttribute("class","card");
+    catchYouid.appendChild(youDiv);
+
+    var catchYoudiv = document.getElementById("youDiv");
+
+    var youTopnum = document.createElement("span");
+    youTopnum.setAttribute("class","topnum");
+    youTopnum.innerHTML = you[0].num;
+    catchYoudiv.appendChild(youTopnum);
+
+    var youTopsuit = document.createElement("span");
+    youTopsuit.setAttribute("class","topsuit");
+    youTopsuit.innerHTML = you[0].suit;
+    catchYoudiv.appendChild(youTopsuit);
+
+    var youBtmsuit = document.createElement("span");
+    youBtmsuit.setAttribute("class","btmsuit");
+    youBtmsuit.innerHTML = you[0].suit;
+    catchYoudiv.appendChild(youBtmsuit);
+
+    var youBtmnum = document.createElement("span");
+    youBtmnum.setAttribute("class","btmnum");
+    youBtmnum.innerHTML = you[0].num;
+    catchYoudiv.appendChild(youBtmnum);
+
+}
--- js/indian.js
+++ js/indian.js
@@ -1,16 +1,13 @@
+
+
 var deck = [];
 var suit = ["HEART", "DIA", "CLUB", "SPADE"];
 
 deckPush(deck); //デッキをプッシュ
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 1caf8bbac8bf4bf7b9c1e248696183515740b7ff
 
 var me = [];
 var you = [];
 
-battle(me, you, deck);
+battle(me, you, deck);//ドローして、if文
 
-shuffle(deck);
+// shuffle(deck);
