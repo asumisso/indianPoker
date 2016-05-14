5b8c2e4d9980883321548bacb3b6572e41ad66e5
hayatostreme <dothazyukko@gmail.com>
2016-05-14 15:25:04 +0900

    hayato function.js change

--- js/function.js
+++ js/function.js
@@ -1,5 +1,4 @@
 function deckPush(deck) {
-    // >>>>>>> 5f6707572bd7547a8d1c9cc7c6d71acc0ca55625
 
     for (var i = 0; i < suit.length; i++) {
         for (var j = 0; j < 13; j++) {
@@ -14,51 +13,23 @@ function deckPush(deck) {
 
 }
 
-function draw(name) {
-
-    var rand = Math.floor(Math.random() * deck.length);
-    name.push(deck[rand]);
-    deck.splice(rand, 1);
-}
-
 function shuffle(array) {
     for (var i = 0; i < 15; i++) {
         var rand = Math.floor(Math.random() * array.length);
         console.log(array[rand]); //デバッグ
-
-<<<<<<< HEAD
-  var rand = Math.floor( Math.random() * deck.length);    
-  me = deck[rand];
-  deck.splice(rand,1);
-  rand = Math.floor( Math.random() * deck.length);    
-  you = deck[rand];
-  deck.splice(rand,1);
-  
-  if(me.num > you.num){
-      document.write("あなたの勝利です！");
-    }else if (me.num < you.num) {
-      document.write("あなたの負けです…");
-    }else if (me.num == you.num){
-      document.write("引き分け");
-    }
   }
 
-
-  function shuffle(deck) {
-  var n = deck.length, t, i;
-=======
-    }
 }
->>>>>>> 5a855787d1b30d36e0cbf97ef7407ec6e5001bf2
 
-function draw(me, you) {
+function battle(me, you) {
 
+  function draw(name) {
     var rand = Math.floor(Math.random() * deck.length);
-    me = deck[rand];
+    name = deck[rand];
     deck.splice(rand, 1);
-    rand = Math.floor(Math.random() * deck.length);
-    you = deck[rand];
-    deck.splice(rand, 1);
+  }
+    draw(me);
+    draw(you);
 
     if (me.num > you.num) {
         document.write("あなたの勝利です！");
@@ -69,23 +40,4 @@ function draw(me, you) {
     } else {
         document.write("おかしい");
     }
-
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-console.log(deck);
-=======
-console.log(deck);
->>>>>>> d4d37df2f880cb97b16a3233069a683c4fce9adc
->>>>>>> 5f6707572bd7547a8d1c9cc7c6d71acc0ca55625
-
-
-
-console.log("aaaaa");
->>>>>>> e5503a2a7b4dcb15eb83ef036cdfb09d50edcdee
-=======
-
-
-
-    console.log("aaaaa");
->>>>>>> 5a855787d1b30d36e0cbf97ef7407ec6e5001bf2
+  }
